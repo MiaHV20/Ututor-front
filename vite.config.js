e4913dbf64b8258785c3108be7be9286ae5c745d
MiaHV20 <mia.hurtado@utec.edu.pe>
2025-10-08 22:10:36 -0500

    Nuevito

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // 👇 Esta parte agrega el proxy para redirigir las peticiones de /api al balanceador
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://lb-prod-656082431.us-east-1.elb.amazonaws.com',
+        changeOrigin: true,
+        secure: false, // permite SSL self-signed si el balanceador no tiene certificado público
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
